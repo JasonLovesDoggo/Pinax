d261f591a2b18f865dfc19ca4d49664a58c69019
Jason <git@jasoncameron.dev>
2024-07-15 19:17:08 -0400

    refactor
    Signed-off-by: Jason <git@jasoncameron.dev>

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,9 @@
-import {Component, createSignal, onCleanup, onMount} from 'solid-js'
+import {createSignal, onMount} from 'solid-js'
 import solidLogo from './assets/solid.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-
-interface CountUpProps {
-    start: number;
-    end: number;
-    duration: number; // in seconds
-    easing?: (t: number, b: number, c: number, d: number) => number;
-}
-
-const CountUp: Component<CountUpProps> = (props) => {
-    const [current, setCurrent] = createSignal(props.start);
-
-    const easeOutExpo = (t: number, b: number, c: number, d: number) =>
-        c * (-Math.pow(2, -10 * t / d) + 1) + b;
-
-    onMount(() => {
-        const startTime = performance.now();
-        const step = () => {
-            const timeElapsed = performance.now() - startTime;
-            const progress = Math.min(timeElapsed / (props.duration * 1000), 1);
-
-            const easingFn = props.easing || easeOutExpo;
-            const easedValue = easingFn(progress, props.start, props.end - props.start, 1);
-            setCurrent(easedValue);
-
-            if (progress < 1) {
-                requestAnimationFrame(step);
-            }
-        };
-
-        requestAnimationFrame(step);
-    });
-
-    onCleanup(() => {
-        // This isn't strictly necessary, but good practice for potential long-running animations
-        window.cancelAnimationFrame(requestAnimationFrame(() => {
-        }));
-    });
-
-    return <span>{current().toFixed(0)}</span>; // Display as integer
-};
+// @ts-ignore
+import CountUp from "./components/CountUp";
 
 function App() {
   const [count, setCount] = createSignal(0)
@@ -66,7 +27,7 @@ function App() {
       </div>
       <h1>Vite + Solid</h1>
       <div class="card">
-        <span>Count is {<CountUp end={count()} start={0} duration={40}/>}</span>
+        <span>Count is {<CountUp end={count()} start={0} duration={20}/>}</span>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
