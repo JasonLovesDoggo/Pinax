2c21b07d36a7daaac64836bb41fca2557bfbb239
Jason <git@jasoncameron.dev>
2024-07-16 02:48:02 -0400

    added proper layouts
    Signed-off-by: Jason <git@jasoncameron.dev>

---
+++ src/Layout.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import {createContext, JSX} from "solid-js";
+import {Container, CssBaseline} from "@suid/material";
+import NavBar from "@components/NavBar";
+import Footer from "@components/Footer";
+
+createContext()
+
+// export const [theme, setTheme] = createSignal({});
+
+
+const Layout = (props: { children: JSX.Element[] }) => {
+    return (
+        <>
+            <CssBaseline/>
+            <NavBar/>
+            <Container sx={{height: "100vh", width: "100vw"}} as={"main"} disableGutters maxWidth={false}>
+                {props.children}
+            </Container>
+            <Footer/>
+        </>
+    )
+}
+export default Layout;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 /* @refresh reload */
 import {render} from "solid-js/web";
+import {Route, Router} from "@solidjs/router";
+import NotFound from "@components/pages/Errors";
+import Home from "@components/pages/Home";
+import Layout from "./Layout";
 
-import "./index.css";
-import App from "./App";
-
-const root = document.getElementById("root");
-
-render(() => <App />, root!);
+render(
+    () => (
+        // @ts-ignore
+        <Router root={Layout}>
+            <Route path="/" component={Home}/>
+            <Route path="/hello-world" component={() => <h1>Hello World!</h1>}/>
+            <Route path="*404" component={NotFound}/>
+        </Router>
+    ),
+    document.getElementById("app")!
+);
