c1905c913f4fcea204f6c4bffd1b6a2ffdb0735e
Jason <git@jasoncameron.dev>
2024-07-15 22:43:13 -0400

    add SUID (MUI)
    Signed-off-by: Jason <git@jasoncameron.dev>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import {defineConfig} from "vite";
 import solid from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
+import suidPlugin from "@suid/vite-plugin";
 
 export default defineConfig({
-  plugins: [solid(), tsconfigPaths()],
+  plugins: [suidPlugin(), solid(), tsconfigPaths()],
+  build: {
+    target: "esnext",
+  },
 });
