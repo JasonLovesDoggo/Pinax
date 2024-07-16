356a8c04be750435ca59d2e664e8c8bf6e8762a3
Jason <git@jasoncameron.dev>
2024-07-16 09:21:57 -0400

    fmt
    Signed-off-by: Jason <git@jasoncameron.dev>

--- src/components/common/base.tsx
+++ src/components/common/base.tsx
@@ -2,29 +2,31 @@ import {JSX} from "solid-js";
 import {Box as SBox, Theme} from "@suid/material";
 
 export const A = (props: {
-    href: string;
-    newTab?: boolean;
-    children: JSX.Element;
+  href: string;
+  newTab?: boolean;
+  children: JSX.Element;
 }) => {
-    if (!props.newTab) props.newTab = true;
-    return (
-        <a
-            href={props.href}
-            target={props.newTab ? "_blank" : "_self"}
-            style={{"text-decoration": "none", color: "white"}}
-        >
-            {props.children}
-        </a>
-    );
+  if (!props.newTab) props.newTab = true;
+  return (
+    <a
+      href={props.href}
+      target={props.newTab ? "_blank" : "_self"}
+      style={{ "text-decoration": "none", color: "white" }}
+    >
+      {props.children}
+    </a>
+  );
 };
 
-
 export const BBox = (props: { sx: any; children: JSX.Element }) => {
-    return (
-        <SBox
-            sx={{
-                borderColor: (theme: Theme) => theme.palette.primary.main,
-                ...props.sx,
-            }}>{props.children}</SBox>
-    );
-}
+  return (
+    <SBox
+      sx={{
+        borderColor: (theme: Theme) => theme.palette.primary.main,
+        ...props.sx,
+      }}
+    >
+      {props.children}
+    </SBox>
+  );
+};
