4b1d12a8ad9865bb3f5a49e05c0ae29bb2749921
Jason <git@jasoncameron.dev>
2024-07-15 23:05:30 -0400

    add SUID (MUI)
    Signed-off-by: Jason <git@jasoncameron.dev>

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,31 @@ import "./App.css";
 import ViewCount from "@components/ViewCount";
 import CommitInfo from "@components/CommitInfo";
 import WakaTime from "@components/WakaTime";
+// import {ThemeProvider} from "@suid/material";
+import {createContext} from "solid-js";
+
+createContext()
+
+// export const [theme, setTheme] = createSignal(BaseTheme);
+
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1>Portfolio</h1>
-        <p>My personal portfolio website</p>
-        <WakaTime />
-        <ViewCount duration={1.1} />
-        <CommitInfo />
-      </div>
-    </>
-  );
+
+
+    return (
+        <>
+            {/*<ThemeProvider theme={theme()}>*/}
+
+                <div>
+                    <h1>Portfolio</h1>
+                    <p>My personal portfolio website</p>
+                    <WakaTime/>
+                    <ViewCount duration={1.1}/>
+                    <CommitInfo/>
+                </div>
+            {/*</ThemeProvider>*/}
+        </>
+    );
 }
 
 export default App;
