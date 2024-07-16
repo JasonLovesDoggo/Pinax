9840b21b2a79e7764392f06647dfdea3da6ec154
Jason <git@jasoncameron.dev>
2024-07-16 01:12:27 -0400

    added (a disabled ts analyzer)
    Signed-off-by: Jason <git@jasoncameron.dev>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
 import suidPlugin from "@suid/vite-plugin";
+import { compression } from "vite-plugin-compression2";
+// import analyzer from "vite-bundle-analyzer";
 
 export default defineConfig({
-  plugins: [suidPlugin(), solid(), tsconfigPaths()],
+  plugins: [
+    suidPlugin(),
+    // analyzer(),
+    solid(),
+    tsconfigPaths(),
+    compression({
+      algorithm: "gzip",
+      exclude: [/\.(br)$ /, /\.(gz)$/],
+    }),
+    compression({
+      algorithm: "brotliCompress",
+      exclude: [/\.(br)$ /, /\.(gz)$/],
+    }),
+  ],
   build: {
     target: "esnext",
   },
