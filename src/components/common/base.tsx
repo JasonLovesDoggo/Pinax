27a48e3565e3125f4a94be90f802460d93f25f77
Jason <git@jasoncameron.dev>
2024-07-15 21:13:50 -0400

    Created abstract A component
    Signed-off-by: Jason <git@jasoncameron.dev>

---
+++ src/components/common/base.tsx
@@ -0,0 +1,18 @@
+import {JSX} from "solid-js";
+
+export const A = (props: {
+  href: string;
+  newTab?: boolean;
+  children: JSX.Element;
+}) => {
+  if (!props.newTab) props.newTab = true;
+  return (
+    <a
+      href={props.href}
+      target={props.newTab ? "_blank" : "_self"}
+      style={{ "text-decoration": "none", color: "white" }}
+    >
+      {props.children}
+    </a>
+  );
+};
