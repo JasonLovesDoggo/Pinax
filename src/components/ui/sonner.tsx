b7aec25c3723f085db23ca0de22b664707a73b33
Jason Cameron <git@jasoncameron.dev>
2024-10-12 00:54:18 -0400

    theme sonar
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,26 +1,26 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import { Toaster as Sonner } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
       className="toaster group"
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
+            "group toast group-[.toaster]:bg-base group-[.toaster]:text-text group-[.toaster]:border-overlay2" +
+            " group-[.toaster]:shadow-lg",
+          title: "group-[.toast]:text-text group-[.toast]:font-semibold",
+          description: "group-[.toast]:text-subtext1",
           actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+            "group-[.toast]:!bg-subtext1  group-[.toast]:!text-surface0 group-[.toast]:hover:!scale-105",
           cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+            "group-[.toast]:!bg-subtext1  group-[.toast]:!text-surface0",
+          closeButton:
+            '"group-[.toast]:!bg-subtext1  group-[.toast]:!text-surface0 group-[.toast]:hover:opacity-100',
         },
       }}
       {...props}
