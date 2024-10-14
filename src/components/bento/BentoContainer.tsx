74f6cf6efb285c665c8ae2a7b5148ba83962e40a
Jason Cameron <git@jasoncameron.dev>
2024-10-14 17:36:43 -0400

    Fix bento container & add auto arrange
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/components/bento/BentoContainer.tsx
+++ src/components/bento/BentoContainer.tsx
@@ -1,16 +1,23 @@
 "use client";
-import { Mosaic, MosaicWindow } from "react-mosaic-component";
+import {
+  createBalancedTreeFromLeaves,
+  getLeaves,
+  Mosaic,
+  MosaicNode,
+  MosaicWindow,
+} from "react-mosaic-component";
 
 // import "react-mosaic-component/react-mosaic-component.css";
 import Chats from "@/components/bento/cards/about";
 import "@/styles/bento.css";
+import { ReactNode, useState } from "react";
 
 // import "@blueprintjs/core/lib/css/blueprint.css";
 // import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 export type ViewTypes = "a" | "b" | "c" | "d" | "e";
 
-const ELEMENT_MAP: Record<ViewTypes, JSX.Element> = {
+const ELEMENT_MAP: Record<ViewTypes, ReactNode> = {
   a: <Chats />,
   b: <Chats />,
   c: <Chats />,
@@ -26,29 +33,62 @@ const listOfIds = {
   e: "What",
 };
 
+export interface BentoState {
+  currentNode: MosaicNode<string> | null;
+}
+
+const initialState: BentoState = {
+  currentNode: {
+    direction: "row",
+    first: {
+      direction: "column",
+      first: "a",
+      second: "b",
+    },
+    second: {
+      direction: "column",
+      first: "c",
+      second: "d",
+    },
+    splitPercentage: 10,
+  },
+};
+
 const BentoContainer = () => {
+  const [state, setState] = useState<BentoState>(initialState);
+
+  const onChange = (currentNode: BentoState["currentNode"]) => {
+    setState({ currentNode });
+  };
+
+  const autoArrange = () => {
+    const leaves = getLeaves(state.currentNode);
+
+    setState({
+      currentNode: createBalancedTreeFromLeaves(leave),
+    });
+  };
+
   return (
-    <Mosaic
-      renderTile={(id, path) => (
-        <MosaicWindow<ViewTypes> path={path} title={listOfIds[id as ViewTypes]}>
-          {ELEMENT_MAP[id as ViewTypes]}
-        </MosaicWindow>
-      )}
-      initialValue={{
-        direction: "row",
-        first: {
-          direction: "column",
-          first: "a",
-          second: "b",
-        },
-        second: {
-          direction: "column",
-          first: "c",
-          second: "d",
-        },
-        splitPercentage: 60,
-      }}
-    />
+    <>
+      <button onClick={autoArrange}>Auto Arrange</button>
+      <Mosaic
+        renderTile={(id, path) => (
+          <MosaicWindow<ViewTypes>
+            path={path}
+            title={listOfIds[id as ViewTypes]}
+          >
+            {ELEMENT_MAP[id as ViewTypes]}
+          </MosaicWindow>
+        )}
+        onRelease={(n) => {
+          console.log("released", n);
+        }}
+        onChange={onChange}
+        initialValue={undefined}
+        value={state.currentNode}
+      />
+    </>
   );
 };
 
