4489d02d7e3abfeba65e2e81decd4f8f03e5d36b
Jason Cameron <git@jasoncameron.dev>
2024-11-19 21:14:10 -0500

    Add link preview card
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  images: {
+    domains: [
+      "api.microlink.io", // Microlink Image Preview
+    ],
+  },
   redirects: async () => {
     return [
       {
