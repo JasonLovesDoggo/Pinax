ae7dc27afdc8c5a141ddb18fe7d208332ffa9845
Jason Cameron <git@jasoncameron.dev>
2024-12-14 15:37:22 -0500

    feat: add new Radix UI and React Icon packages for shadcn
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,2 +1,4 @@
 export { default as Theme } from "./theme";
 export { default as Location } from "./location";
+export { default as Spotify } from "./spotify";
+export { default as Skills } from "./skills";
