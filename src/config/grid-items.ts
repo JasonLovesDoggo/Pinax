ed642b06db67797b02283ffb81a2b26d1d829e84
Jason Cameron <git@jasoncameron.dev>
2024-12-14 13:47:11 -0500

    add missing import to grid
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/config/grid-items.ts
+++ src/config/grid-items.ts
@@ -1,19 +1,20 @@
 import * as Cards from "@/components/cards";
+import { ComponentType } from "react";
 
 export interface GridItem {
   i: string;
-  component: React.ComponentType;
+  component: ComponentType;
 }
 
 export const gridItems: GridItem[] = [
   // { i: 'description', component: GridComponents.Description },
   { i: "location", component: Cards.Location },
-  // { i: 'project-1', component: GridComponents.FirstProject },
+  { i: "spotify", component: Cards.Spotify },
   // { i: 'linkedin', component: GridComponents.LinkedIn },
   { i: "theme", component: Cards.Theme },
   // { i: 'project-2', component: GridComponents.SecondProject },
   // { i: 'spotify', component: GridComponents.Spotify },
-  // { i: 'project-3', component: GridComponents.ThirdProject },
+  { i: "skills", component: Cards.Skills },
   // { i: 'article', component: GridComponents.Article },
   // { i: 'contact', component: GridComponents.Contact },
 ];
