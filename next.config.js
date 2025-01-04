bb1cd2295e5a3b5936ab50aeef5ba89387319429
Jason Cameron <git@jasoncameron.dev>
2025-01-04 06:04:43 -0500

    Fix: Remove unused R2 domain from Next.js config
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    domains: [process.env.R2_CACHE_DOMAIN],
     remotePatterns: [
       {
         protocol: "https",
